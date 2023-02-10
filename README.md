2fac6fb0cb818796759386b23d3c5b57d6a6171e
root <piotr62@live.fr>
2023-02-10 08:29:10 -0800

    alors

---
+++ README.md
@@ -0,0 +1,1 @@
+basic repo to work and train on ethers.js interaction with UI
--- dapp/script.js
+++ dapp/script.js
@@ -18,32 +18,106 @@ const abi = [
   'function withdraw() returns (bool)',
 ];
 
-const address = '0xc644dCfd21551d3392AC92B0046b750C6DCAC5fA';
-let contract = null;
+const address = '0xcae4911fe80cD787bcDc08e29b0B1bED69B27099';
+let auction = null;
 
-async function init() {
-  if (contract) return;
+async function getAccess() {
+  if (auction) return;
   await provider.send('eth_requestAccounts', []);
   const signer = provider.getSigner();
-  contract = new ethers.Contract(address, abi, signer);
+  auction = new ethers.Contract(address, abi, signer);
+}
+async function bid() {
+  await getAccess();
+  let mybid = document.getElementById('value').value;
+  mybidContract = ethers.BigNumber.from(mybid);
+  await auction.bid(mybidContract).then((error, res) => {
+    if (error) {
+      console.log('error here ' + error);
+      document.getElementById('biding_status').innerHTML =
+        'Think to bidding higher';
+    } else {
+      document.getElementById('biding_status').innerHTML =
+        'Successfull bid, transaction ID' + result;
+    }
+  });
 }
 
-async function bid(val) {
-  await init();
-  var mybid = document.getElementById('value').value;
-  await contract.bid(
-    {
-      value: mybid,
-    },
-    function (err, result) {
-      if (err) {
-        console.log('error is ' + err);
-        document.getElementById('biding_status').innerHTML =
-          'Think to pull out Biftons!';
-      } else {
-        document.getElementById('biding_status').innerHTML =
-          'Successfull bid, transaction ID pour les impots: ' + result;
-      }
-    }
-  );
+async function init() {
+  auction.auction_end(function (error, result) {
+    document.getElementById('auction_end').innerHTML = result;
+  });
+  auction.highestBidder(function (error, result) {
+    document.getElementById('HighestBidder').innerHTML = result;
+  });
+  auction.highestBid(function (error, result) {
+    var bidEther = web3.fromWei(result, 'ether');
+    document.getElementById('HighestBid').innerHTML = bidEther;
+  });
+  auction.STATE(function (error, result) {
+    document.getElementById('STATE').innerHTML = result;
+  });
+  auction.Mycar(function (error, result) {
+    document.getElementById('car_brand').innerHTML = result[0];
+    document.getElementById('registration_number').innerHTML = result[1];
+  });
+  auction.bids(bidder, function (error, result) {
+    var bidEther = web3.fromWei(result, 'ether');
+    document.getElementById('MyBid').innerHTML = bidEther;
+    console.log(bidder);
+  });
 }
+
+var auction_owner = null;
+auction.get_owner(function (error, result) {
+  if (!error) {
+    auction_owner = result;
+    if (bidder != auction_owner) {
+      $('#auction_owner_operations').hide();
+    }
+  }
+});
+
+function cancel_auction() {
+  auction.cancel_auction(function (error, result) {
+    console.log(result);
+  });
+}
+
+function Destruct_auction() {
+  auction.destruct_auction(function (error, result) {
+    console.log(result);
+  });
+}
+
+var BidEvent = auction.BidEvent();
+BidEvent.watch(function (error, result) {
+  if (!error) {
+    $('#eventslog').html(
+      result.args.highestBidder +
+        ' has bidden(' +
+        result.args.highestBid +
+        ' wei)'
+    );
+  } else {
+    console.log(error);
+  }
+});
+
+var CanceledEvent = auction.CanceledEvent();
+CanceledEvent.watch(function (error, result) {
+  if (!error) {
+    $('#eventslog').html(result.args.message + ' at ' + result.args.time);
+  }
+});
+
+const filter = web3.eth.filter({
+  fromBlock: 0,
+  toBlock: 'latest',
+  address: contractAddress,
+  topics: [web3.sha3('BidEvent(address,uint256)')],
+});
+
+filter.get((error, result) => {
+  if (!error) console.log(result);
+});
